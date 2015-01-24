9c2cc51b4f62195bafd475ac9b04ba32de57e7a3
Snuzie <grippe@kth.se>
2015-01-25 00:07:39 +0100

    Height 1.0

--- heightmap.go
+++ heightmap.go
@@ -3,53 +3,118 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"math"
 	"time"
 )
 
 const max float64 = 1.0
-const size int = 32+1
-const min int = 4
 
+const min int = 4
+/*
 func main() {
 
-	rand.Seed( time.Now().UTC().UnixNano())
-	var A [size][size] float64
+	//rand.Seed( time.Now().UTC().UnixNano())
 
-	update(&A,Pair{(size-1)/2,(size-1)/2},(size-1)/2)
+	//size := 9
 
-	printArray(A)
+	s := 17
+	m := make([][]float64, s)
+	for x:=0; x<s;x++ {
+		m[x] = make([]float64, s)
+		for y :=0; y<s; y++ {
+			m[x][y] = float64(x+y)
+		}
+	}
+
+	printArray(m)
+	tm := translateHM(&m, -1, 1)
+	printArray(*tm)
+	
+	//var A [size][size] float64
+
+	//update(&A,Pair{(size-1)/2,(size-1)/2},(size-1)/2)
+	//hm := createHM(size)
+
+	//printArray(*hm)
+
+	
 }
-
+*/
 type Pair struct {
 	row int
 	col int
 }
 
 func createHM(size int) *[][]float64 {
-
+	//const s = size
 	rand.Seed( time.Now().UTC().UnixNano())
-	var A [size][size] float64
+	//var A [size][size] float64
+	A := hmHelper(size)
 
 	update(&A,Pair{(size-1)/2,(size-1)/2},(size-1)/2)
-} 
+	return &A
+}
+
+func translateHM(hm *[][]float64, ox , oy int) *[][]float64 {
+	size := len(*hm)
+	
+	
+	transHm := hmHelper(size)
+	for x:=0+ox; x < size; x++ {
+		for y:=0+oy; y<size;y++ {
+			if !(x < 0 || y < 0 || (x-ox)>=size || (y-oy)>=size) {
+				transHm[x-ox][y-oy] = (*hm)[x][y]
+			}
+		} 
+	}
+
+	printArray(transHm)
+	
+	update(&transHm,Pair{(size-1)/2,(size-1)/2},(size-1)/2)	
+	return &transHm
+}
+
+func hmHelper(size int) [][]float64 {
+
+	hm := make([][]float64, size)
+	for x:=0; x<size; x++ {
+		hm[x] = make([]float64, size)
+		for y:=0; y<size; y++ {
+			hm[x][y] = math.NaN()
+		}
+	}
+	hm[0][0] = 0
+	hm[0][size-1] = 0
+	hm[size-1][0] = 0
+	hm[size-1][size-1] = 0
+	return hm
+}
 
 func get4Mid(p1 Pair, p2 Pair, p3 Pair, p4 Pair) Pair{
 	return Pair{(p1.row+p2.row+p3.row+p4.row)/4,(p1.col+p2.col+p3.col+p4.col)/4}
 }
 
-func getVal(a *[size][size]float64, p Pair) float64 {
+func getVal(a *[][]float64, p Pair) float64 {
+	if math.IsNaN((*a)[p.row][p.col]) {
+		return 0.0
+	}
 	return (*a)[p.row][p.col]
 }
 
-func setVal(a *[size][size]float64, p Pair, val float64) {
-	(*a)[p.row][p.col] = val
+func setVal(a *[][]float64, p Pair, val float64) {
+	if math.IsNaN((*a)[p.row][p.col]) {
+		(*a)[p.row][p.col] = val
+	}
 }
 
-func isOut(p Pair) bool{
+func isOut(p Pair, size int) bool{
 	return p.row < 0 || p.row >= size || p.col < 0 || p.col >= size
 }
 
-func diamond(a* [size][size]float64, p Pair, l int){
+func diamond(a* [][]float64, p Pair, l int){
+
+		size := len(*a)
+		
 		up := Pair{p.row-l,p.col}
 		down := Pair{p.row+l,p.col}
 		left := Pair{p.row,p.col-l}
@@ -60,13 +125,14 @@ func diamond(a* [size][size]float64, p Pair, l int){
 			diamondAvg += (rand.Float64()*max-max/2)*float64(l)/float64(size)
 		}
 
-		if isOut(up) {
+
+		if isOut(up, size) {
 			diamondAvg += (getVal(a, down) + getVal(a, left) + getVal(a, right))/3
-		} else if isOut(down) {
+		} else if isOut(down, size) {
 			diamondAvg += (getVal(a, up) + getVal(a, left) + getVal(a, right))/3
-		} else if isOut(left) {
+		} else if isOut(left, size) {
 			diamondAvg += (getVal(a, up) + getVal(a, down) + getVal(a, right))/3
-		} else if isOut(right) {
+		} else if isOut(right, size) {
 			diamondAvg += (getVal(a, up) + getVal(a, down) + getVal(a, left))/3
 		} else {
 			diamondAvg += (getVal(a, up) + getVal(a, down) + getVal(a, left) + getVal(a, right))/4
@@ -75,7 +141,9 @@ func diamond(a* [size][size]float64, p Pair, l int){
 		setVal(a, p, diamondAvg)
 }
 
-func square(a* [size][size]float64, p Pair, l int){
+func square(a* [][]float64, p Pair, l int){
+		size := len(*a)
+
 		topLeft			:= Pair{p.row-l,p.col-l}
 		topRight		:= Pair{p.row-l,p.col+l}
 		bottomLeft	:= Pair{p.row+l,p.col-l}
@@ -89,7 +157,7 @@ func square(a* [size][size]float64, p Pair, l int){
 		setVal(a, p, squareAvg)
 }
 
-func update(a *[size][size]float64, p Pair, l int) {
+func update(a *[][]float64, p Pair, l int) {
 		//fmt.Println(l, p)
 		
 		if l > 0 {
@@ -115,7 +183,9 @@ func isPowerOfTwo (num int) bool {
 }
 */
 
-func printArray(a[size][size] float64) {
+func printArray(a[][] float64) {
+	size := len(a)
+
 	fmt.Print("[")
 	for i := 0; i < size-1; i++ {
 		fmt.Print("[")
